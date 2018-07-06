fb8c2a938d2be57be8281608841f704b90c85f09
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-06 11:19:26 -0600

    Fix validation

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -4,7 +4,7 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   def receive(data)
-    data["color"] = "#000000" unless code ~= regex
+    data["color"] = "#000000" unless data["color"] =~ regex
     data["name"] = "Default Name" if data["name"].length < 1
     ActionCable.server.broadcast("chat_#{params[:room]}", data)
   end
