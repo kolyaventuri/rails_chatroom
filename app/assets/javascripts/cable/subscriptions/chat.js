ad1bd7efdd104b6dbfaa35890708d46d71a1949c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-06 10:37:02 -0600

    Client side receive code

--- app/assets/javascripts/cable/subscriptions/chat.js
+++ app/assets/javascripts/cable/subscriptions/chat.js
@@ -1,1 +1,13 @@
-App.cable.subscriptions.create({ channel: "ChatChannel" });
+App.chatChannel = App.cable.subscriptions.create(
+  {
+    channel: "ChatChannel",
+    recieved: recieved
+  }
+);
+
+var recieved = function(data) {
+  console.log(data);
+};
+
+App.chatChannel.send({ text: 'Hello World!', user: 'Me' });
+
