e984a1d4a653b2d54e5e703df6f64da743ff2430
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-06 11:14:29 -0600

    Remove unused spec

--- spec/controllers/api/v1/message_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe Api::V1::MessagesController, type: :controller do
-  it 'should broadcast to create when a message is POSTed to /api/v1/messages' do
-    expect { post :create, message: { text: 'Hello, World!', user: 'BillyBob' } }.to
-      have_broadcasted_to('chat').with( { text: 'Hello, World!', user: 'BillyBob' })
-  end
-end
